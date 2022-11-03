a3d5effd5008bbd862b264f1a753d68df7102269
JiaminYuan <ymimi1121@gmail.com>
2022-11-03 11:20:11 -0400

    add and details animal completed

--- app/views/Animal/details.php
+++ app/views/Animal/details.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Edit client</title>
+	<title>Animal details</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
@@ -27,9 +27,18 @@
 		Picture:
 	</dt>
 	<dd>
-		<img src="/images/<?= $data['animal']->profile_pic ?>" style="max-width:200px;max-height:200px" />
+		<img src="/images/blank.jpg" style="max-width:200px;max-height:200px" id="profile_pic_preview" />
 	</dd>
 </dl>
+
+
+
+<script>
+file = "" + "<?= $data['animal']->profile_pic ?>"
+if (file != "") {
+	document.getElementById("profile_pic_preview").src = "/images/" + file;
+}
+</script>
 
 <a href='/Animal/index/<?= $data['owner']->owner_id ?>'>Back to index</a>
 
